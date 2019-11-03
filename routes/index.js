46132cfd0149c49456c149ffc9132bcbd095cdc6
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2019-11-04 00:25:04 +0700

    Included fontawesome assets and completed 'users management' feature

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Bảng điều khiển" });
 });
 router.get("/index.html", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Bảng điều khiển" });
+});
+
+/* GET users page. */
+router.get("/users.html", function(req, res, next) {
+  res.render("users", { title: "Quản lý người dùng" });
+});
+
+/* GET single user page. */
+router.get("/single-user.html", function(req, res, next) {
+  res.render("single-user", { title: "Thông tin người dùng" });
 });
 
 module.exports = router;
